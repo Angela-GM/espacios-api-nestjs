e839548681c30b53270cd40135fc5acc82099372
Angela-GM <angelagm0112@gmail.com>
2023-06-13 15:01:35 +0200

    create servicios

--- src/solicitud/solicitud.module.ts
+++ src/solicitud/solicitud.module.ts
@@ -7,6 +7,6 @@ import { Solicitud } from './entities/solicitud.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([Solicitud])],
   controllers: [SolicitudController],
-  providers: [SolicitudService, SolicitudRepository],
+  providers: [SolicitudService],
 })
 export class SolicitudModule {}
--- src/solicitud/solicitud.service.spec.ts
+++ src/solicitud/solicitud.service.spec.ts
@@ -1,7 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SolicitudService } from './solicitud.service';
+import { CreateSolicitudDto } from './dtos/create-solicitud-dto';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Solicitud } from './entities/solicitud.entity';
 
-const solicitud: any = [
+const solicitudes: any = [
   {
     id: 1,
     nombre: 'Sara',
@@ -20,16 +23,59 @@ const solicitud: any = [
 
 describe('SolicitudService', () => {
   let service: SolicitudService;
+  const mockSolicitudRepositoryService = {
+    find: jest.fn().mockReturnValue(Promise.resolve(solicitudes)),
+    save: jest
+      .fn()
+      .mockImplementation((createSolicitudDto: CreateSolicitudDto) => {
+        return {
+          id: 1,
+          ...createSolicitudDto,
+        };
+      }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SolicitudService],
-    }).compile();
+      providers: [
+        SolicitudService,
+        {
+          provide: getRepositoryToken(Solicitud),
+          useValue: mockSolicitudRepositoryService,
+        },
+      ],
+    })
+      // .overrideProvider(getRepositoryToken(Solicitud))
+      // .useValue(mockSolicitudRepositoryService)
+      .compile();
 
     service = module.get<SolicitudService>(SolicitudService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findAll() should return the array "espacios"', async () => {
+    expect(await service.findAll()).toMatchObject(solicitudes);
+  });
+
+  it('should create an space and return the new solicitud ', async () => {
+    const newSolicitud = {
+      nombre: 'Sara',
+      cargo: 'formador',
+      promocion: 'p7',
+      email: 'sara@gmail.com',
+      tipo: 'masterclass',
+      nombreActividad: 'Taller Testing APIs NestJS',
+      start: new Date('2023-06-15T00:00:00.000Z'),
+      end: new Date('2023-06-15T00:00:00.000Z'),
+      dia: 'Lunes',
+      horaInicio: '9:00 h',
+      horaFin: '13:00 h',
+    };
+    expect(await service.create(newSolicitud)).toMatchObject({
+      id: expect.any(Number),
+    });
   });
 });
